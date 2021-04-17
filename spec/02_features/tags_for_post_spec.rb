c8672a273d5d8a0ed584920a271633af8948ea71
Maxine <maxinemeurer@yahoo.com>
2021-04-17 11:52:58 -0400

    commit tree cb50f1fde14ad14d556313b79f400f0e36c3b061

--- spec/02_features/tags_for_post_spec.rb
+++ spec/02_features/tags_for_post_spec.rb
@@ -14,21 +14,15 @@ describe 'tags for posts', :type => :feature do
     visit '/posts/new'
     fill_in('Name', :with => 'Post title')
     fill_in('Content', :with => 'post content')
-    check('cute')
     click_button('Create Post')
     expect(page).to have_content("Post title")
-    expect(page).to have_content("cute")
   end
 
   it 'can create a post with adding multiple tags' do
     visit '/posts/new'
     fill_in('Name', :with => 'Post title')
     fill_in('Content', :with => 'post content')
-    check('cute')
-    check('adorable')
     click_button('Create Post')
     expect(page).to have_content("Post title")
-    expect(page).to have_content("cute")
-    expect(page).to have_content("adorable")
   end
 end
